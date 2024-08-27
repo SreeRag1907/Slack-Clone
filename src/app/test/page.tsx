2d959ebd74a970f6f351ffc96d8d9793311a9d53
C.SREERAG <99243618+SreeRag1907@users.noreply.github.com>
2024-08-27 13:20:23 +0530

    added the convex-client-provider from the convex

---
+++ src/app/test/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { useQuery } from "convex/react";
+
+export default function Home() {
+  return (
+    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
+      test
+    </main>
+  );
+}

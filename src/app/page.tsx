a9d2a2db840357349452475e841855078d1796be
C.SREERAG <99243618+SreeRag1907@users.noreply.github.com>
2024-08-28 21:16:43 +0530

    added workspaces

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,25 @@
 "use client";
-import { UserButton } from "./auth/components/user-button";
+import { useEffect, useMemo, useState } from "react";
+import { UserButton } from "../features/auth/components/user-button";
+import { useGetWorkspaces } from "../features/workspaces/api/use-get-workspaces";
 export default function Home() {
+  const { data, isLoading } = useGetWorkspaces();
 
+  const workspaceId = useMemo(() => data?.[0]?._id, [data]);
+
+  useEffect(() => {
+    if (isLoading) return;
+
+    if (workspaceId) {
+      console.log("rediretc to workspaces");
+    } else {
+      console.log("open creation modal");
+    }
+  }, [workspaceId, isLoading]);
 
   return (
     <div>
-      <UserButton/>
+      <UserButton />
     </div>
   );
 }

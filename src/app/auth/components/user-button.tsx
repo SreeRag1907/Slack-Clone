2c06a641a11560add83b743003f4fe54753f0402
C.SREERAG <99243618+SreeRag1907@users.noreply.github.com>
2024-08-28 11:07:58 +0530

    added little changes

--- src/app/auth/components/user-button.tsx
+++ src/app/auth/components/user-button.tsx
@@ -12,10 +12,14 @@ import {
   DropdownMenuTrigger,
 } from "@/src/components/ui/dropdown-menu";
 import { useCurrentUser } from "../hooks/use-current-user";
-import { Loader } from "lucide-react";
+import { Loader, LogOut } from "lucide-react";
+import { useAuthActions } from "@convex-dev/auth/react";
 
 export const UserButton = () => {
-  const { data, isLoading } = useCurrentUser();
+
+    const {signOut} = useAuthActions();
+
+    const { data, isLoading } = useCurrentUser();
 
   if (isLoading) {
     return <Loader className='size-4 animate-spin text-muted-foreground' />;
@@ -24,20 +28,23 @@ export const UserButton = () => {
     return null;
   }
 
-  const { image, name, email } = data;
+  const { image, name } = data;
 
-  //1;48:47
+  const avatarFallback = name!.charAt(0).toUpperCase();
 
   return (
     <DropdownMenu modal={false}>
       <DropdownMenuTrigger className='outline-none relative'>
         <Avatar className='size-10 hover:opacity-75 transition'>
-          <AvatarImage />
-          <AvatarFallback></AvatarFallback>
+          <AvatarImage alt={name} src={image} />
+          <AvatarFallback className="bg-blue-500 text-white">{avatarFallback}</AvatarFallback>
         </Avatar>
       </DropdownMenuTrigger>
       <DropdownMenuContent align='center' side='right' className='w-60'>
-        <DropdownMenuItem>Sign out</DropdownMenuItem>
+        <DropdownMenuItem onClick={()=>signOut()} className="h-10 cursor-pointer">
+          <LogOut className='size-4 mr-2 ' />
+          Log Out
+        </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,10 @@
 "use client";
-
-import { useAuthActions } from "@convex-dev/auth/react";
-import { Button } from "../components/ui/button";
 import { UserButton } from "./auth/components/user-button";
 export default function Home() {
 
-  const {signOut} = useAuthActions();
 
   return (
     <div>
-      Logged In!!
       <UserButton/>
     </div>
   );

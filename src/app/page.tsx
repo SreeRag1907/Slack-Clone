2a3de594c44bb1226e81f3c3cb002720f0ff257a
C.SREERAG <99243618+SreeRag1907@users.noreply.github.com>
2024-08-29 12:34:00 +0530

    updated the layout and page for the workspaces

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,10 @@
 import { useEffect, useMemo, useState } from "react";
 import { UserButton } from "../features/auth/components/user-button";
 import { useGetWorkspaces } from "../features/workspaces/api/use-get-workspaces";
+import { useCreateWorkspaceModal } from "../features/store/use-create-workspace-modal";
 export default function Home() {
+  const [open, setOpen] = useCreateWorkspaceModal();
+
   const { data, isLoading } = useGetWorkspaces();
 
   const workspaceId = useMemo(() => data?.[0]?._id, [data]);
@@ -12,10 +15,10 @@ export default function Home() {
 
     if (workspaceId) {
       console.log("rediretc to workspaces");
-    } else {
-      console.log("open creation modal");
+    } else if (!open) {
+      setOpen(true);
     }
-  }, [workspaceId, isLoading]);
+  }, [workspaceId, isLoading ,open,setOpen]);
 
   return (
     <div>
@@ -23,3 +26,5 @@ export default function Home() {
     </div>
   );
 }
+
+//2:01
